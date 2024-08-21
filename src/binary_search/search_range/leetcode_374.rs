0241f622854c533aa15661c619c848aba01d472d
VYAR <vadim.d.yarovoy@yandex.ru>
2024-08-21 16:07:51 +0300

    fix: Remove LeetCode 374 doctest

--- src/binary_search/search_range/leetcode_374.rs
+++ src/binary_search/search_range/leetcode_374.rs
@@ -1,12 +1,3 @@
-/**
- * Forward declaration of guess API.
- * @param  num   your guess
- * @return 	     -1 if num is higher than the picked number
- *			      1 if num is lower than the picked number
- *               otherwise return 0
- * unsafe fn guess(num: i32) -> i32 {}
- */
-
 #[allow(dead_code)]
 #[allow(non_snake_case)]
 unsafe fn guessNumber(n: i32) -> i32 {
